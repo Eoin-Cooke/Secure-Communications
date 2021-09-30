8f277b452642f10526bc30fdd9ddbda84ace610a
Eoin-Cooke <cookes002@gmail.com>
2021-09-30 09:22:46 -0400

    message

---
+++ CryptoHack/introToCH-Ex6.py
@@ -0,0 +1,12 @@
+from Cryptodome.Util.number import long_to_bytes
+
+
+longNumber = 1515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+byte = long_to_bytes(longNumber)
+
+print(byte)
+
+#for some reason, this doesn't work. It says that the module 
+#Cryptodome.Util.number doesn't exist even though I have 
+#Crypotdome installed. I even reinstalled it and it still doesn't work.
