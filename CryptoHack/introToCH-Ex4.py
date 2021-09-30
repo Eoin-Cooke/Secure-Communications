fe951131329361a1a3aebc7226a16b25878daaf0
Eoin-Cooke <cookes002@gmail.com>
2021-09-30 09:35:13 -0400

    test

--- CryptoHack/introToCH-Ex4.py
+++ CryptoHack/introToCH-Ex4.py
@@ -1,3 +1,3 @@
-b_hex = bytearray.fromhex("63 72 79 70 74 6f 7b 59 6f 75 5f 77 69 6c 6c 5f 62 65 5f 77 6f 72 6b 69 6e 67 5f 77 69 74 68 5f 68 65 78 5f 73 74 72 69 6e 67 73 5f 61 5f 6c 6f 74 7d")
+a_hex = bytearray.fromhex("63 72 79 70 74 6f 7b 59 6f 75 5f 77 69 6c 6c 5f 62 65 5f 77 6f 72 6b 69 6e 67 5f 77 69 74 68 5f 68 65 78 5f 73 74 72 69 6e 67 73 5f 61 5f 6c 6f 74 7d")
 
-print(b_hex)
+print(a_hex)
